2c8c7ab883bae986cd7f4c64f78b7e2d7be00452
Nve Hounkponou Orphee <orpheenve@hotmail.com>
2020-10-21 17:57:51 +0200

    first commit

---
+++ precache-manifest.d5a9c05933ccc3a69fa21a5fa68b57d9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ecf6ccea319fb04421",
+    "url": "/css/about.f05af134.css"
+  },
+  {
+    "revision": "af6c05dfe2809b753b4f",
+    "url": "/css/app.822a0205.css"
+  },
+  {
+    "revision": "e012297e1a7baa5db073",
+    "url": "/css/chunk-vendors.0a56aaea.css"
+  },
+  {
+    "revision": "075992050bce2c5854dd",
+    "url": "/css/makeburger.15919f6f.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "62e5d3ec6309cb36f34ad919ef210a13",
+    "url": "/img/3567.62e5d3ec.jpg"
+  },
+  {
+    "revision": "e815484a1db8a10b6873aefa78a9fe3c",
+    "url": "/img/logo.e815484a.png"
+  },
+  {
+    "revision": "fb30d5d52190b45268d509461573bd13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46ecf6ccea319fb04421",
+    "url": "/js/about.15b021ac.js"
+  },
+  {
+    "revision": "af6c05dfe2809b753b4f",
+    "url": "/js/app.453c727a.js"
+  },
+  {
+    "revision": "e012297e1a7baa5db073",
+    "url": "/js/chunk-vendors.e85205d0.js"
+  },
+  {
+    "revision": "075992050bce2c5854dd",
+    "url": "/js/makeburger.20b7e8a5.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
